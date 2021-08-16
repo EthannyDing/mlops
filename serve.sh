217a3dda6a4057621bfed0af62aeb27d6f7485dd
Ethan Ding <43589728+EthannyDing@users.noreply.github.com>
2021-08-16 10:14:13 -0400

    add bash files to serve model and fetch predictions

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 export MLFLOW_TRACKING_URI="postgresql://postgres:knowtions12345@localhost:5432/models"
-mlflow models serve -m "models:/XGboostModel/1"
+mlflow models serve -m "models:/SimpleXGboostModel/1" --no-conda
